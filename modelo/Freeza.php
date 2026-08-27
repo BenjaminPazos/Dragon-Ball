2d068fed3c040d6162737442d67e65bb046a42ab
BenjaminPazos <20251FOZ0020031@estudantes.ifpr.edu.br>
2026-08-27 15:42:06 -0300

    Add files via upload

---
+++ modelo/Freeza.php
@@ -0,0 +1,40 @@
+<?php
+require_once("IGuerreiro.php");
+
+class Freeza implements IGuerreiro
+{
+    public function atacar() {
+        $dados = $this->ataque . "!!! \n";
+        return $dados;
+    }
+    public function transformar() {
+        $base = $this->transformacao;
+        return $base;
+    }
+    private string $ataque;
+    private string $transformacao;
+
+    public function getAtaque(): string
+    {
+        return $this->ataque;
+    }
+
+    public function setAtaque(string $ataque): self
+    {
+        $this->ataque = $ataque;
+
+        return $this;
+    }
+
+    public function getTransformacao(): string
+    {
+        return $this->transformacao;
+    }
+
+    public function setTransformacao(string $transformacao): self
+    {
+        $this->transformacao = $transformacao;
+
+        return $this;
+    }
+}
